5758d0f913585815250c0582c2a4f8f1be75cd79
Joao Monte <jotavemonte@gmail.com>
2018-04-04 00:29:46 -0300

    Update lorem.py

--- lorem.py
+++ lorem.py
@@ -1,9 +1,9 @@
-﻿import numpy as np
+from random import randint
 
 def lorem_generator(paragraphs_number):
     """This function aims to build a lorem generator for the DinofauroFexBot"""
-    conectives_list =   ['do','vah','ki','in','pro','stra','gma','tri','tong','ne','et','me','tos','bahs','noni','male'] #Aleatórios
-    words_list      =   ['avada','kedavra','lumus','maximus','accio','aguamenti','alarte','ascendare','alohomora','aparecium','apparate',
+    CONECTIVES_LIST =   ['do','vah','ki','in','pro','stra','gma','tri','tong','ne','et','me','tos','bahs','noni','male'] #Aleatórios
+    WORDS_LIST      =   ['avada','kedavra','lumus','maximus','accio','aguamenti','alarte','ascendare','alohomora','aparecium','apparate',
                          'aqua','eructo','ascendio','avis','avifors','arresto','momentum','arania','exumai','baubillious','bombarda','maxima','brackium','emendo',
                          'calvario','cantis','carpe','retractum','cave','inimicum','cistem','aperio','colloportus','colloshoo','colovaria','cofundo','ventus','duo','vara',
                          'verto','verdimillious']
@@ -13,17 +13,17 @@ def lorem_generator(paragraphs_number):
     numberof_paragraphs = range(paragraphs_number)
     conectives_rr       = 3 #Could variate randomly. It will be analised later
     lorem               = ""
-    len_conectives      = len(conectives_list)
-    len_words           = len(words_list)
+    len_conectives      = len(CONECTIVES_LIST)
+    len_words           = len(WORDS_LIST)
     for i in numberof_paragraphs:
         conective_flag      = 1
-        paragraphslen_rr    = np.random.randint(80,150)
+        paragraphslen_rr    = randint(80,150)
         for j in range(paragraphslen_rr):
             comma_rr = np.random.randint(4,15)
             if conective_flag == 3:
-                lorem += conectives_list[np.random.randint(0,len_conectives)] + " "
+                lorem += CONECTIVES_LIST[randint(0,len_conectives)] + " "
 		conective_flag = 1
-            lorem = lorem + words_list[np.random.randint(0,len_words)] + " "
+            lorem = lorem + WORDS_LIST[randint(0,len_words)] + " "
             conective_flag += 1
         lorem += ".\n\n"
     return lorem
